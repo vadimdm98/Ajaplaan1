482c288ee65f82774343ab7025f0afe8cb018bef
Kasutaja <Kasutaja@L0000194974>
2023-04-11 11:56:06 +0300

    Add project files.

---
+++ Ajaplaan/Ajaplaan/AjaplaanPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Ajaplaan
+{
+    public partial class AjaplaanPage : ContentPage
+    {
+        TimeSpan _triggerTime;
+
+        public  AjaplaanPage()
+        {
+            InitializeComponent();
+        }
+
+        private void _timePicker_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            _triggerTime = _timePicker.Time;
+           
+            if (_triggerTime.Hours.ToString() == "4")
+            {
+                LabelInfo.Text = "Get up";
+                imageforpicker.Source = "getup.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "5")
+            {
+                LabelInfo.Text = "Brush your teeth";
+                imageforpicker.Source = "teeth.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "6")
+            {
+                LabelInfo.Text = "Have breakfast";
+                imageforpicker.Source = "breakfast.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "7")
+            {
+                LabelInfo.Text = "Take a shower";
+                imageforpicker.Source = "shower.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "8")
+            {
+                LabelInfo.Text = "Get dressed";
+                imageforpicker.Source = "dressed.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "9")
+            {
+                LabelInfo.Text = "Go to school";
+                imageforpicker.Source = "school.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "13")
+            {
+                LabelInfo.Text = "Study English";
+                imageforpicker.Source = "study.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "15")
+            {
+                LabelInfo.Text = "Have a lunch";
+                imageforpicker.Source = "lunch.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "16")
+            {
+                LabelInfo.Text = "Exercise";
+                imageforpicker.Source = "exercise.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "17")
+            {
+                LabelInfo.Text = "Do your homework";
+                imageforpicker.Source = "homework.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "19")
+            {
+                LabelInfo.Text = "Cook dinner";
+                imageforpicker.Source = "dinner.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "20")
+            {
+                LabelInfo.Text = "Watch TV";
+                imageforpicker.Source = "tv.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "21")
+            {
+                LabelInfo.Text = "Take a bath";
+                imageforpicker.Source = "bath.png";
+            }
+            else if (_triggerTime.Hours.ToString() == "22")
+            {
+                LabelInfo.Text = "Go to bed";
+                imageforpicker.Source = "bed.png";
+            }
+
+            ButtonTagasi.Clicked += ButtonTagasi_Clicked;
+        }
+
+        private async void ButtonTagasi_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Ajaplaan/Ajaplaan/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ajaplaan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        List<ContentPage> pages = new List<ContentPage>() { new AjaplaanPage() };
+        List<string> tekstid = new List<string> { "Ava Ajaplaan leht" };
+
+        public MainPage()
+        {
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.Aqua
+            };
+            for (int i = 0; i < pages.Count; i++)
+            {
+                Button button = new Button
+                {
+                    Text = tekstid[i],
+                    TabIndex = i,
+                    BackgroundColor = Color.Fuchsia,
+                    TextColor = Color.Black
+                };
+                st.Children.Add(button);
+                button.Clicked += Navig_funktsion;
+            }
+            Content = st;
+        }
+
+        private async void Navig_funktsion(object sender, EventArgs e)
+        {
+            Button btn = sender as Button; //(Button)sender
+            await Navigation.PushAsync(pages[btn.TabIndex]);
+        }
+    }
+}
